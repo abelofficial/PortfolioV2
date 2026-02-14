f885eec30a7329fb3652f2f3053f00b09dc59781
Abel Tariku Sintaro <abelofficial94@gmail.com>
2026-02-14 16:18:50 +0100

    chore(ci): fix broken env veriable

--- src/services/datoCMS.ts
+++ src/services/datoCMS.ts
@@ -18,11 +18,11 @@ export const request = async <TResponse, TVariables = Record<string, unknown>>({
 }: IDatoRequestProps<TVariables>): Promise<TResponse> => {
   const client = new GraphQLClient('https://graphql.datocms.com', {
     headers: {
-      Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+      Authorization: `Bearer ${process.env.ACCOUNT_DATOCMS_API_TOKEN}`,
       ...(includeDrafts && { 'X-Include-Drafts': 'true' }),
       ...(excludeInvalid && { 'X-Exclude-Invalid': 'true' }),
     },
-    
+
     fetch: (url, config) =>
       fetch(url, {
         ...config,
